e8f98204ab8263deac9e156b16ca0a4ffdd509b0
Still Hsu <341464@gmail.com>
2018-06-23 04:11:18 +0800

    Bump to version 1.1.x
    + Added the functionality of building description for managed references.
    + Tweaked the overall logic and added summary for each method.
    + Article XPath now uses //article[@id='_content'] instead of class-based guesses.

--- DocFX.Plugin.DescriptionGenerator/DescriptionPostProcessor.cs
+++ DocFX.Plugin.DescriptionGenerator/DescriptionPostProcessor.cs
@@ -4,6 +4,7 @@ using System.Composition;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using HtmlAgilityPack;
 using Microsoft.DocAsCode.Common;
 using Microsoft.DocAsCode.Plugins;
@@ -13,11 +14,12 @@ namespace DocFX.Plugin.DescriptionGenerator
     [Export(nameof(DescriptionPostProcessor), typeof(IPostProcessor))]
     public class DescriptionPostProcessor : IPostProcessor
     {
-        private int _savedFiles = 0;
-        private const int FixedDescriptionLength = 135;
+        private const int FixedDescriptionLength = 150;
         private const string FullStopDelimiter = ". ";
+        private int _savedFiles;
 
-        public ImmutableDictionary<string, object> PrepareMetadata(ImmutableDictionary<string, object> metadata) => metadata;
+        public ImmutableDictionary<string, object> PrepareMetadata(ImmutableDictionary<string, object> metadata) =>
+            metadata;
 
         public Manifest Process(Manifest manifest, string outputFolder)
         {
@@ -26,49 +28,83 @@ namespace DocFX.Plugin.DescriptionGenerator
                                      ?.InformationalVersion ??
                                  Assembly.GetExecutingAssembly().GetName().Version.ToString();
             Logger.LogInfo($"Version: {versionInfo}");
-            foreach (var manifestItem in manifest.Files.Where(x=>x.DocumentType == "Conceptual"))
+            foreach (var manifestItem in manifest.Files.Where(x => !string.IsNullOrEmpty(x.SourceRelativePath)))
             {
-                foreach (var manifestItemOutputFile in manifestItem.OutputFiles)
+                Logger.LogVerbose($"Document type for {manifestItem.SourceRelativePath} is {manifestItem.DocumentType}.");
+                foreach (var manifestItemOutputFile in manifestItem.OutputFiles.Where(x =>
+                    !string.IsNullOrEmpty(x.Value.RelativePath)))
                 {
                     string sourcePath = Path.Combine(manifest.SourceBasePath, manifestItem.SourceRelativePath);
                     string outputPath = Path.Combine(outputFolder, manifestItemOutputFile.Value.RelativePath);
-                    WriteDescriptionTag(sourcePath, outputPath);
+                    if (manifestItem.DocumentType == "Conceptual")
+                        WriteDescriptionTag(sourcePath, outputPath, ArticleType.Conceptual);
+                    if (manifestItem.DocumentType == "ManagedReference")
+                        WriteDescriptionTag(sourcePath, outputPath, ArticleType.Reference);
                 }
             }
-            Logger.LogInfo($"Added description tags to {_savedFiles} conceptual articles.");
+
+            Logger.LogInfo($"Added description tags to {_savedFiles} items.");
             return manifest;
         }
 
-        private void WriteDescriptionTag(string sourcePath, string outputPath)
+        /// <summary>
+        ///     Injects the description tag according to the type of article.
+        /// </summary>
+        /// <param name="sourcePath">The original article path.</param>
+        /// <param name="outputPath">The output path.</param>
+        /// <param name="type">The type of document.</param>
+        private void WriteDescriptionTag(string sourcePath, string outputPath, ArticleType type)
         {
             Logger.LogVerbose($"Processing metadata from {sourcePath} to {outputPath}...");
             var htmlDoc = new HtmlDocument();
             htmlDoc.Load(outputPath);
-            var articleNode = htmlDoc.DocumentNode.SelectSingleNode("//article[contains(@class, 'content wrap')]/p");
-            if (articleNode == null)
+            
+            string descriptionText = string.Empty;
+            switch (type)
             {
-                Logger.LogVerbose("ArticleNode containing a valid paragraph not found, returning...");
-                return;
+                case ArticleType.Conceptual:
+                    var articleInnerText = htmlDoc.DocumentNode.SelectSingleNode("//article[@id='_content']/p")?.InnerText;
+                    if (string.IsNullOrEmpty(articleInnerText)) return;
+
+                    int articlePunctuationPos = articleInnerText.IndexOf(FullStopDelimiter, StringComparison.Ordinal);
+                    descriptionText = articlePunctuationPos <= FixedDescriptionLength && articlePunctuationPos > 0
+                        ? articleInnerText.Remove(articlePunctuationPos + FullStopDelimiter.Length).Trim()
+                        : Truncate(articleInnerText, FixedDescriptionLength, "...");
+                    break;
+                case ArticleType.Reference:
+                    var sb = new StringBuilder();
+
+                    var memberName = htmlDoc.DocumentNode.SelectSingleNode("//article[@id='_content']/h1")?.InnerText;
+                    if (!string.IsNullOrEmpty(memberName)) sb.Append(memberName.Trim());
+
+                    var memberDescription = htmlDoc.DocumentNode.SelectSingleNode("//div[contains(@class, 'level0 summary')]/p")?.InnerText;
+                    if (!string.IsNullOrEmpty(memberDescription)) sb.Append($" {char.ToLowerInvariant(memberDescription[0]) + memberDescription.Substring(1)}");
+
+                    descriptionText = sb.ToString();
+                    break;
             }
 
-            if (string.IsNullOrWhiteSpace(articleNode.InnerText))
-            {
-                Logger.LogVerbose("Article paragraph not found or empty, returning...");
-                return;
-            }
+            if (string.IsNullOrEmpty(descriptionText)) return;
+            AppendDescription(htmlDoc, descriptionText).Save(outputPath);
+            _savedFiles++;
+        }
 
-            var articlePunctuationPos = articleNode.InnerText.IndexOf(FullStopDelimiter, StringComparison.Ordinal);
-            var articleDescription = articlePunctuationPos <= FixedDescriptionLength && articlePunctuationPos > 0
-                ? articleNode.InnerText.Remove(articlePunctuationPos + FullStopDelimiter.Length).Trim()
-                : Truncate(articleNode.InnerText, FixedDescriptionLength, "...");
-
+        /// <summary>
+        ///     Appends the description text into the meta tag of the document.
+        /// </summary>
+        /// <param name="htmlDoc">The document to be modified.</param>
+        /// <param name="descriptionText">The text to append.</param>
+        /// <returns>
+        ///     The modified <see cref="HtmlDocument" />.
+        /// </returns>
+        private HtmlDocument AppendDescription(HtmlDocument htmlDoc, string descriptionText)
+        {
             var headerNode = htmlDoc.DocumentNode.SelectSingleNode("//head");
             var metaDescriptionNode = htmlDoc.CreateElement("meta");
             metaDescriptionNode.SetAttributeValue("name", "description");
-            metaDescriptionNode.SetAttributeValue("content", articleDescription);
+            metaDescriptionNode.SetAttributeValue("content", descriptionText);
             headerNode.AppendChild(metaDescriptionNode);
-            htmlDoc.Save(outputPath);
-            _savedFiles++;
+            return htmlDoc;
         }
 
         /// <summary>
@@ -80,7 +116,7 @@ namespace DocFX.Plugin.DescriptionGenerator
         /// </remarks>
         /// <seealso href="https://github.com/Humanizr/Humanizer/" />
         /// <seealso href="https://github.com/Humanizr/Humanizer/blob/master/LICENSE" />
-        public string Truncate(string value, int length, string truncationString)
+        private string Truncate(string value, int length, string truncationString)
         {
             if (value == null)
                 return null;
@@ -95,5 +131,14 @@ namespace DocFX.Plugin.DescriptionGenerator
                 ? value.Substring(0, length - truncationString.Length) + truncationString
                 : value;
         }
+    }
+
+    /// <summary>
+    ///     Defines the type of article.
+    /// </summary>
+    internal enum ArticleType
+    {
+        Conceptual,
+        Reference
     }
 }
